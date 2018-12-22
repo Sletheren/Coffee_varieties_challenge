d3bf13f24a39df1dcae0c7ca35af8eac29e90796
Unknown <rakhma.abdelghafour@gmail.com>
2018-12-22 01:35:28 +0100

    Adding Request validation schemas

---
+++ Helpers/schemas.js
@@ -0,0 +1,65 @@
+const Joi = require('joi')
+
+/**
+ * 
+ * Schemas for our main routes!
+ * Each route can have 3 schemas (params, query and body)
+ * each schema sets validation rules to the data that request contains
+ * 
+ */
+
+const Schemas = {
+  all: {
+    params: {},
+    query: {
+      name: Joi.string(),
+      bean_size: Joi.string(),
+      quality_potential: Joi.string(),
+      yield: Joi.string(),
+      disease_resistancy: Joi.array().items(Joi.any()),
+      producing_countries: Joi.array().items(Joi.string())
+    },
+    body: {}
+  },
+  find: {
+    params: {
+      id: Joi.number().required()
+    },
+    query: {},
+    body: {}
+  },
+  create: {
+    params: {},
+    query: {},
+    body: {
+      name: Joi.string().required(),
+      bean_size: Joi.string().required(),
+      quality_potential: Joi.string().required(),
+      yield: Joi.string().required(),
+      disease_resistancy: Joi.array().items(Joi.object()).required(),
+      producing_countries: Joi.array().items(Joi.string()).required()
+    }
+  },
+  update: {
+    params: {
+      id: Joi.number().required()
+    },
+    query: {},
+    body: {
+      name: Joi.string(),
+      bean_size: Joi.string(),
+      quality_potential: Joi.string(),
+      yield: Joi.string(),
+      disease_resistancy: Joi.array().items(Joi.object()),
+      producing_countries: Joi.array().items(Joi.string())
+    }
+  },
+  remove: {
+    params: {
+      id: Joi.number().required()
+    },
+    query: {},
+    body: {}
+  }
+}
+module.exports = Schemas
