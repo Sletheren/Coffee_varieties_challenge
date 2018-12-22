470b0d8d37bbf36ca1ce859e7340659f6577b8de
Unknown <rakhma.abdelghafour@gmail.com>
2018-12-22 01:34:59 +0100

    Adding Response Helper

---
+++ Helpers/responses.js
@@ -0,0 +1,34 @@
+/**
+ * Helper functions to normalize the responses we send to the end-user
+ * 
+ * @param {String} message 
+ * @param {Object} data 
+ */
+
+const success = (data, message) => ({
+  status: 200,
+  message: message || 'OK',
+  data: data
+})
+
+const notFound = () => ({
+  status: 404,
+  message: 'NOT_FOUND'
+})
+
+const genericError = (message) => ({
+  status: 400,
+  message: message
+})
+
+const serverError = () => ({
+  status: 500,
+  message: 'SERVER_ERROR'
+})
+
+module.exports = {
+  success,
+  notFound,
+  genericError,
+  serverError
+}
