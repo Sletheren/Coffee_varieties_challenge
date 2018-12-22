3733468835205fe0860f1002787d667a4f1b10e3
Unknown <rakhma.abdelghafour@gmail.com>
2018-12-22 15:10:39 +0100

    Cleaning up

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -6,25 +6,7 @@ const parseParamId = (req, res, next) => {
     req.params.id = (+req.params.id)
   next()
 }
-const parseParamObject = (req, res, next) => {
-  if (req.query) {
-    Object.keys(req.query).forEach(q => {
-      if (Array.isArray(req.query[q])) {
-        req.query[q] = req.query[q].map(item => {
-          try {
-            const object = JSON.parse(item)
-            return object
-          } catch (e) {
-            return item
-          }
-        })
-      }
-    })
-  }
-  next()
-}
 
 module.exports = {
-  parseParamId,
-  parseParamObject
+  parseParamId
 }
